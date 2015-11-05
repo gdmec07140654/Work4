558fef49370ecb643dd72f1caa8de0cc3c3118c9
Zheng Weizhan <907652300@qq.com>
2015-11-05 22:10:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/work4/ContactsTable.java
@@ -0,0 +1,143 @@
+package com.example.administrator.work4;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.Vector;
+
+/**
+ * Created by Administrator on 2015/11/2.
+ */
+public class ContactsTable {
+    private final static String TABLENAME = "contactsTable";
+    private MyDB db;
+
+    public ContactsTable(Context context) {
+        db = new MyDB(context);
+        if (!db.isTableExists(TABLENAME)) {
+            String createTableSql = "CREATE TABLE IF NOT EXISTS" +
+                    TABLENAME + "(id_DB integer primary key AUTOINCREMENT," +
+                    User.NAME + "VARCHAR," +
+                    User.MOBILE + "VARCHAR," +
+                    User.DANWEI + "VARCHAR," +
+                    User.QQ + "VARCHAR," +
+                    User.ADDRESS + "VARCHAR)";
+            db.createTable(createTableSql);
+        }
+    }
+    public  boolean addData(User user){
+        ContentValues values=new ContentValues();
+        values.put(User.NAME,user.getName());
+        values.put(User.MOBILE,user.getMobile());
+        values.put(User.QQ,user.getQq());
+        values.put(User.DANWEI,user.getDanwei());
+        values.put(User.ADDRESS,user.getAddress());
+        return db.save(TABLENAME,values);
+    }
+    public User[] getAllUser() {
+        Vector<User> v = new Vector<User>();
+        Cursor cursor = null;
+        try {
+            cursor = db.find("select * from" + TABLENAME, null);
+            while (cursor.moveToNext()) {
+                User temp = new User();
+                temp.setId_DB(cursor.getInt(cursor.getColumnIndex("id_DB")));
+                temp.setAddress(cursor.getString(cursor.getColumnIndex(User.ADDRESS)));
+                temp.setQq(cursor.getString(cursor.getColumnIndex(User.QQ)));
+                temp.setDanwei(cursor.getString(cursor.getColumnIndex(User.DANWEI)));
+                temp.setName(cursor.getString(cursor.getColumnIndex(User.NAME)));
+                temp.setMobile(cursor.getString(cursor.getColumnIndex(User.MOBILE)));
+                v.add(temp);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            db.close();
+        }
+        if (v.size() > 0) {
+            return v.toArray(new User[]{});
+        } else {
+            User[] users = new User[1];
+            User user = new User();
+            user.setName("无结果");
+            users[0] = user;
+            return users;
+        }
+    }
+
+    public User getUserByID(int id) {
+        Cursor cursor = null;
+        try {
+            cursor = db.find("select * from" + TABLENAME + "where id_DB", new String[]{id + ""});
+            User temp = new User();
+            cursor.moveToNext();
+            temp.setId_DB(cursor.getInt(cursor.getColumnIndex("id_DB")));
+            temp.setAddress(cursor.getString(cursor.getColumnIndex(User.ADDRESS)));
+            temp.setQq(cursor.getString(cursor.getColumnIndex(User.QQ)));
+            temp.setDanwei(cursor.getString(cursor.getColumnIndex(User.DANWEI)));
+            temp.setName(cursor.getString(cursor.getColumnIndex(User.NAME)));
+            temp.setMobile(cursor.getString(cursor.getColumnIndex(User.MOBILE)));
+            return temp;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            db.closeConnection();
+        }
+        return null;
+    }
+    public boolean updateUser(User user){
+        ContentValues values=new ContentValues();
+        values.put(User.NAME,user.getName());
+        values.put(User.MOBILE,user.getMobile());
+        values.put(User.QQ,user.getQq());
+        values.put(User.DANWEI,user.getDanwei());
+        values.put(User.ADDRESS,user.getAddress());
+        return db.update(TABLENAME,values,"id_db=?",new String[]{user.getId_DB()+""});
+    }
+    public User[] findUserByKey(String key) {
+        Vector<User> v = new Vector<>();
+        Cursor cursor = null;
+        try {
+            cursor = db.find("select * from" + TABLENAME + "where"
+                    + User.NAME + "like '%" + key + "%'" +
+                    "or" + User.MOBILE + "like '%" + key + "%'" +
+                    "or" + User.QQ + "like '%" + key + "%'", null);
+            while (cursor.moveToNext()) {
+                User temp = new User();
+                temp.setId_DB(cursor.getInt(cursor.getColumnIndex("id_DB")));
+                temp.setAddress(cursor.getString(cursor.getColumnIndex(User.ADDRESS)));
+                temp.setQq(cursor.getString(cursor.getColumnIndex(User.QQ)));
+                temp.setDanwei(cursor.getString(cursor.getColumnIndex(User.DANWEI)));
+                temp.setName(cursor.getString(cursor.getColumnIndex(User.NAME)));
+                temp.setMobile(cursor.getString(cursor.getColumnIndex(User.MOBILE)));
+                v.add(temp);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            db.close();
+        }
+        if (v.size() > 0) {
+            return v.toArray(new User[]{});
+        } else {
+            User[] users = new User[1];
+            User user = new User();
+            user.setName("无结果");
+            users[0] = user;
+            return users;
+        }
+    }
+    public boolean deleteByUser(User user){
+        return db.delete(TABLENAME,"id_db=?",new String[]{user.getId_DB()+""});
+    }
+}
---
+++ app/src/main/java/com/example/administrator/work4/User.java
@@ -0,0 +1,69 @@
+package com.example.administrator.work4;
+
+/**
+ * Created by Administrator on 2015/11/1.
+ */
+public class User {
+    public final static String NAME ="name";
+    public final static String DANWEI ="danwei";
+    public final static String MOBILE ="mobile";
+    public final static String QQ ="qq";
+    public final static String ADDRESS ="address";
+
+    private String name;
+    private String danwei;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDanwei() {
+        return danwei;
+    }
+
+    public void setDanwei(String danwei) {
+        this.danwei = danwei;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getId_DB() {
+        return id_DB;
+    }
+
+    public void setId_DB(int id_DB) {
+        this.id_DB = id_DB;
+    }
+
+    private String mobile;
+    private String qq;
+    private String address;
+    private int id_DB=-1;
+
+}
